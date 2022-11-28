88dd9f09769e88706a8d2c4e6818cf11be316879
hari48721 <84365408+hari48721@users.noreply.github.com>
2022-11-28 17:47:32 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://www.waveplusweb.com/ehari';
+$config['base_url'] = 'https://www.waveplusweb.com/hari-github';
 
 /*
 |--------------------------------------------------------------------------
